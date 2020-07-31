6bb8e7de4f1e5ab0142f5a3079971cd7bcf96e7e
WIN-0FKKBPC828E\User <neox56@gmail.com>
2020-07-31 14:47:42 +0400

    feat: add pictureSize.js

--- src/js/main.js
+++ src/js/main.js
@@ -9,6 +9,7 @@ import { showMoreStyles } from './modules/showMoreStyles';
 import { calc } from './modules/calc';
 import { changeModalState } from './modules/changeModalState';
 import { filter } from './modules/filter';
+import { pictureSize } from './modules/pictureSize';
 
 window.addEventListener('DOMContentLoaded', () => {
   modals();
@@ -23,4 +24,5 @@ window.addEventListener('DOMContentLoaded', () => {
   showMoreStyles('.button-styles', '#styles .row');
   calc('#size', '#material', '#options', '.promocode', '.calc-price');
   filter();
+  pictureSize('.sizes-block');
 });
